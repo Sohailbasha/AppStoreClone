a04878a756fb2bc74f0dbf7ab5c920252bc387e5
Sohailbasha <shlbsh94@gmail.com>
2019-04-08 15:23:44 -0400

    implementing delegate methods.

--- AppStoreClone/Apps Search/AppsSearchController.swift
+++ AppStoreClone/Apps Search/AppsSearchController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class AppsSearchController: UICollectionViewController {
 
+    fileprivate var cellId = "searchCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
     }
     
     init() {
@@ -23,6 +24,23 @@ class AppsSearchController: UICollectionViewController {
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+}
+
+extension AppsSearchController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 250)
     }
     
 }
