b352264ddd41c122d3811309a2afb37232d4990a
Sohailbasha <shlbsh94@gmail.com>
2019-04-08 13:28:19 -0400

    Add new icon assets. Add tab bar icons to each tab.

--- AppStoreClone/Tab Bar/BaseTabBarController.swift
+++ AppStoreClone/Tab Bar/BaseTabBarController.swift
@@ -15,22 +15,30 @@ class BaseTabBarController: UITabBarController {
         
         let redViewController = UIViewController()
         redViewController.view.backgroundColor = .red
-        redViewController.tabBarItem.title = "red"
+        redViewController.navigationItem.title = "Today"
         let redNavController = UINavigationController(rootViewController: redViewController)
+        redNavController.navigationBar.prefersLargeTitles = true
         redNavController.tabBarItem.title = "RED"
+        redNavController.tabBarItem.image = UIImage(named: "today_icon")
 
         
         let blueViewController = UIViewController()
         blueViewController.view.backgroundColor = .blue
-        blueViewController.tabBarItem.title = "blue"
+        blueViewController.navigationItem.title = "Apps"
         let blueNavController = UINavigationController(rootViewController: blueViewController)
+        blueNavController.navigationBar.prefersLargeTitles = true
         blueNavController.tabBarItem.title = "BLUE"
+        blueNavController.tabBarItem.image = UIImage(named: "apps")
+
 
         let yellowViewController = UIViewController()
         yellowViewController.view.backgroundColor = .yellow
-        yellowViewController.tabBarItem.title = "yellow"
+        yellowViewController.navigationItem.title = "Search"
         let yellowNavController = UINavigationController(rootViewController: yellowViewController)
+        yellowNavController.navigationBar.prefersLargeTitles = true
         yellowNavController.tabBarItem.title = "YELLOW"
+        yellowNavController.tabBarItem.image = UIImage(named: "search")
+
         
         viewControllers = [
             redNavController,
