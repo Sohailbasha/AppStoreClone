1062df1822bf297bcf965dc57a69d7a24eb77f7b
Sohailbasha <shlbsh94@gmail.com>
2019-04-08 13:07:48 -0400

    Initial Commit

---
+++ AppStoreClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreClone
+//
+//  Created by Ilias Basha on 4/8/19.
+//  Copyright © 2019 sohail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
