4ab74fe9b62286b9e302325fd5872b31aef39eb8
Sohailbasha <shlbsh94@gmail.com>
2019-04-08 13:20:10 -0400

    add tab bar titles to the navigation controllers

--- AppStoreClone/Tab Bar/BaseTabBarController.swift
+++ AppStoreClone/Tab Bar/BaseTabBarController.swift
@@ -15,17 +15,27 @@ class BaseTabBarController: UITabBarController {
         
         let redViewController = UIViewController()
         redViewController.view.backgroundColor = .red
+        redViewController.tabBarItem.title = "red"
+        let redNavController = UINavigationController(rootViewController: redViewController)
+        redNavController.tabBarItem.title = "RED"
+
         
         let blueViewController = UIViewController()
         blueViewController.view.backgroundColor = .blue
-        
+        blueViewController.tabBarItem.title = "blue"
+        let blueNavController = UINavigationController(rootViewController: blueViewController)
+        blueNavController.tabBarItem.title = "BLUE"
+
         let yellowViewController = UIViewController()
         yellowViewController.view.backgroundColor = .yellow
+        yellowViewController.tabBarItem.title = "yellow"
+        let yellowNavController = UINavigationController(rootViewController: yellowViewController)
+        yellowNavController.tabBarItem.title = "YELLOW"
         
         viewControllers = [
-            redViewController,
-            blueViewController,
-            yellowViewController
+            redNavController,
+            blueNavController,
+            yellowNavController
         ]
     }
 }
