d6810edb1ba057f15f5a96083d3da92e9cfea412
Sohailbasha <shlbsh94@gmail.com>
2019-04-08 15:23:29 -0400

    Add new file for custom collection view cell.

---
+++ AppStoreClone/Apps Search/SearchResultCell.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResultCell.swift
+//  AppStoreClone
+//
+//  Created by Ilias Basha on 4/8/19.
+//  Copyright © 2019 sohail. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UICollectionViewCell {
+    
+}
