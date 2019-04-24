022ffe267ee4c8ced835448874df9efef8b41229
Sohailbasha <shlbsh94@gmail.com>
2019-04-24 12:37:09 -0400

    Refactor AppsController and register new cell AppsController.

--- AppStoreClone/Apps/AppsController.swift
+++ AppStoreClone/Apps/AppsController.swift
@@ -11,9 +11,29 @@ import UIKit
 
 class AppsController: BaseListController {
     
+    fileprivate let cellId = "appsCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .yellow
+        
+        collectionView.register(AppsGroupCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+}
+
+extension AppsController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 250)
     }
     
 }
