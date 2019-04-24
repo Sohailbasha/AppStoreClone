090d9b372385c942fd1c529f14539456b8f2a36a
Sohailbasha <shlbsh94@gmail.com>
2019-04-24 12:23:52 -0400

    Create AppsController. add to list of view controllers in tab bar.

---
+++ AppStoreClone/Apps/AppsController.swift
@@ -0,0 +1,28 @@
+//
+//  AppsController.swift
+//  AppStoreClone
+//
+//  Created by Ilias Basha on 4/24/19.
+//  Copyright © 2019 sohail. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppsController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .yellow
+    }
+    
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
