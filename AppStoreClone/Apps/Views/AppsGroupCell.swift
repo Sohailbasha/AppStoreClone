5164a081c5b0d2208bcce4134adb9526ed1bcfe1
Sohailbasha <shlbsh94@gmail.com>
2019-04-24 12:36:43 -0400

    add new cell AppsGroupCell

---
+++ AppStoreClone/Apps/Views/AppsGroupCell.swift
@@ -0,0 +1,23 @@
+//
+//  AppsGroupCell.swift
+//  AppStoreClone
+//
+//  Created by Ilias Basha on 4/24/19.
+//  Copyright © 2019 sohail. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppsGroupCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .purple
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
