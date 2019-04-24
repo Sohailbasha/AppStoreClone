b5befbea5b17a2c2d3b738e4a4198fe4611d5d49
Sohailbasha <shlbsh94@gmail.com>
2019-04-24 12:52:55 -0400

    add new collection view controller to nest inside of collection view cell.

---
+++ AppStoreClone/Apps/AppsHorizontalController.swift
@@ -0,0 +1,19 @@
+//
+//  AppsHorizontalController.swift
+//  AppStoreClone
+//
+//  Created by Ilias Basha on 4/24/19.
+//  Copyright © 2019 sohail. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppsHorizontalController: BaseListController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .gray
+    }
+    
+}
