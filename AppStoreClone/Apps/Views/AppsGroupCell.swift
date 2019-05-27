500d641ce1258475a10158b017d29f6941cee87f
Sohailbasha <shlbsh94@gmail.com>
2019-05-26 19:05:56 -0500

    Add left insets for titleLabel

--- AppStoreClone/Apps/Views/AppsGroupCell.swift
+++ AppStoreClone/Apps/Views/AppsGroupCell.swift
@@ -17,9 +17,9 @@ class AppsGroupCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .purple
+        
         addSubview(titleLabel)
-        titleLabel.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor)
+        titleLabel.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor, padding: .init(top: 0, left: 16, bottom: 0, right: 0))
         
         addSubview(horizontalController.view)
         horizontalController.view.anchor(top: titleLabel.bottomAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
